68e1448fe9c822a5d18e7a88bc7960d72248efa7
bosco <bosco@boscos-MacBook-Pro.local>
2020-03-26 14:27:06 -0400

    up to 151 implement of nav toggle drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
@@ -6,15 +6,34 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => (
-    <Aux>
+class Layout extends Component {
 
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children} 
-        </main>
-    </Aux>
-);
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    render () {
+
+        return(
+        <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer open={this.state.showSideDrawer} 
+                        closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children} 
+            </main>
+        </Aux>
+        )
+    }
+}
+
+export default Layout;
